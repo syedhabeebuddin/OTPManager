a579ffa41de7592c07bfce5f11c7833903089c11
Habeebuddin <Habeebuddin@DESKTOP-5S9FUHG>
2023-12-03 22:51:33 +0530

    Added UnitTests.

--- src/OTPManager/OTPManager.UnitTests.API/AuthenticationControllerTests.cs
+++ src/OTPManager/OTPManager.UnitTests.API/AuthenticationControllerTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using OTPManager.API.Controllers;
 using OTPManager.Application.Services;
+using OTPManager.Domain.Models;
 
 namespace OTPManager.UnitTests.API
 {
@@ -33,6 +34,30 @@ namespace OTPManager.UnitTests.API
                 actualResult.Should().NotBeNull();
                 actualResult.Value.Should().BeOfType<string>();
                 actualResult.Value.Should().Be(code);
+            }
+        }
+
+        [Fact]
+        public void Verify2FACode_Should_Return_True()
+        {
+            //Arrange
+            var code = "112233";
+            var phone = "1111122222";
+            _otpAuthenticatorMock.Setup(x => x.IsLimitExceeded(It.IsAny<string>())).Returns(false);
+            _otpAuthenticatorMock.Setup(x => x.GenerateCode(It.IsAny<string>())).Returns(code);
+            _otpAuthenticatorMock.Setup(x => x.VerifyCode(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+            var request = new VerifyCodeRequest { Phone = phone, Code = code };
+
+            //Act
+            var authenticationController = new AuthenticationController(_otpAuthenticatorMock.Object);            
+            var actualResult = authenticationController.Verify2FACode(request) as OkObjectResult;
+
+            //Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().NotBeNull();
+                actualResult.Value.Should().BeOfType<bool>();
+                actualResult.Value.Should().Be(true);
             }
         }
     }
